c7e2fb510bb7f30d143bbfe3ff3e15fc1c512853
Tuomas <me@tuomas.id>
2026-02-13 13:46:01 +0200

    First video + media preloading

---
+++ src/pages/_landing/_utils/showcaseContent.tsx
@@ -0,0 +1,12 @@
+import chatPreview from '../_images/chat.png';
+import drawPreview from '../_images/draw.png';
+import chatPreviewVideo from '../_videos/chat.mp4';
+
+// We specify what to preload for showcases here
+export const preloadContent = [
+  { href: chatPreview.src, as: 'image', type: `image/${chatPreview.format}` },
+  { href: drawPreview.src, as: 'image', type: `image/${drawPreview.format}` },
+  { href: chatPreviewVideo, as: 'video', type: 'video/mp4' },
+];
+
+export { chatPreview, drawPreview, chatPreviewVideo };
